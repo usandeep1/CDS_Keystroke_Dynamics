19e90d11a9a07d89ac7f624c16377be19f232c3c
Tech Kuo <thk42@cornell.edu>
2015-02-28 16:22:31 -0500

    Basic front end code

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+    var numSlotsFilled = 0;
+
+    $('#passcode').submit(function() {
+        return false;
+    });    
+
+    $('input[type=submit]').click(function() {
+        // find the next empty slot and fill it with a circle
+        $('#successMsg').text('');
+        for (var i=1; i <= 4; i++){
+            var nextSlot = $('li:nth-of-type(' + i + ')');
+            if (nextSlot.text() !== 'O'){
+                nextSlot.text('O');
+                numSlotsFilled++;
+                break;
+            }
+        }
+        // clear the slots if the passcode is complete
+        if (numSlotsFilled === 4){
+            for (var i=1; i <= 4; i++){
+                var nextSlot = $('li:nth-of-type(' + i + ')');
+                nextSlot.text('____');
+            }
+            numSlotsFilled = 0;
+            $('#successMsg').text('Thanks!');
+        }
+    });
+
+});
