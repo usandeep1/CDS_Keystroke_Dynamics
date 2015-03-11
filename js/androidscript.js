bf32232b0ddf84e80dea5f08de456b56bb69d8d1
Sandeep Uppaluri <sandeep@dhcp-gs-1313.eduroam.cornell.edu>
2015-03-10 22:33:23 -0400

    test

--- js/androidscript.js
+++ js/androidscript.js
@@ -18,12 +18,13 @@ $(document).ready(function(){
     
     function tap_start(elem) 
     {
-    	alert ( elem.attr ("id" ) );
-  		// var x = evt.pageX - evt.target.offset().left;
-		// var y = evt.pageY - evt.target.offset().top;
+    	
+  		var x = elem.offset().left; //evt.pageX - evt.target.offset().left;
+		var y = elem.offset().top;  //evt.pageY - evt.target.offset().top;
 		// console.log('start2: ' = $x + ' , ' + $y );
   		// alert ( 'start: ' = $x + ' , ' + $y );
   		// console.log('start: ' = $x + ' , ' + $y );
+  		alert ( 'start: ' + x + ',' + y );
     }
 
     function tap_end(elem) 
