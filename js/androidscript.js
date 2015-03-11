a037468a2bfa9c72c40a95cd202a7e9fea96a674
Sandeep Uppaluri <sandeep@dhcp-gs-1313.eduroam.cornell.edu>
2015-03-10 23:32:40 -0400

    test

--- js/androidscript.js
+++ js/androidscript.js
@@ -4,74 +4,32 @@ $(document).ready(function(){
     var Attempt = Parse.Object.extend("Attempt");
     
     var attempt = new Attempt();
-    attempt.set('start_times', []);
-    attempt.set('end_times', []);
-    attempt.set('start_x_coords', []);
-    attempt.set('start_y_coords', []);
+    attempt.set('start_times', [])
+    attempt.set('end_times', [])
+    attempt.set('start_x_coords', [])
+    attempt.set('start_y_coords', [])
+    
+    function log_tap_start(evt) 
+    {
+    	console.log ( elem.attr ('id') );
+    	console.log ( 'pageX: ' + evt.originalEvent.pageX );
+    	console.log ( 'elem.offset().top: ' + elem.offset().top );
+  		// var x = elem.offset().left; //evt.pageX - evt.target.offset().left;
+		// var y = elem.offset().top;  //evt.pageY - evt.target.offset().top;
+    }
+
+    function log_tap_end(evt) 
+    {
+        console.log ('end');
+    }
 
     var numSlotsFilled = 0;
 
-    $('.number').bind('touchstart', function() {
-    	console.log('touch start'); 
-    	// console.log ( 'e.originalEvent.touches[0].pageX;: ' + evt.originalEvent.touches[0].pageX);
-    	// console.log ( 'e.originalEvent.pageX;: ' + evt.originalEvent.pageX;);
-  		// var x = elem.offset().left; //evt.pageX - evt.target.offset().left;
-		// var y = elem.offset().top;  //evt.pageY - evt.target.offset().top;
-  		// attempt.add('start_times',(new Date()).getTime());
+    $('.number').bind('touchstart', function(evt) { 
+    	log_tap_start(evt); 
     });
-
-    function log_tap_start(evt) 
-    {
-    	console.log ( 'e.originalEvent.touches[0].pageX;: ' + evt.originalEvent.touches[0].pageX);
-    	console.log ( 'e.originalEvent.pageX;: ' + evt.originalEvent.pageX;);
-  		// var x = elem.offset().left; //evt.pageX - evt.target.offset().left;
-		// var y = elem.offset().top;  //evt.pageY - evt.target.offset().top;
-  		attempt.add('start_times',(new Date()).getTime());
-    }
-
-    $('.number').bind('touchend', function() { 
-        console.log ('end');
-        // attempt.add('end_times',(new Date()).getTime());
-
-
-    	$('#successMsg', context).text('');
-    	// fill in the next empty slot with a circle
-        var nextSlot = $('#input_circles li:nth-of-type(' + (numSlotsFilled + 1) + ')');
-        nextSlot.text('O');
-        numSlotsFilled++;
-        
-        // clear all the slots if the passcode is complete
-        if (numSlotsFilled === 4){
-            $('#input_circles li').text('____');
-            numSlotsFilled = 0;
-            
-            // print a success message on completion
-            $('#successMsg', context).text('Thanks!');
-            attempt.save(null, {
-                success: function(object) {
-                    console.log('attempt successfully saved');
-                },
-                error: function(model, error) {
-                    console.log('attempt failed to save');
-                }
-            });
-        }
-    });
-
-    function log_tap_end(elem) 
-    {
-        console.log ('end');
-        attempt.add('end_times',(new Date()).getTime());
-    }
-
-    /// CODE FOR HANDLING BACKSPACE /// 
-    $('#backspace').bind('touchend', function() { 
-        // get the last slot that was filled in and clear it
-        if (numSlotsFilled !== 0) {
-            var lastSlot = $('#input_circles li:nth-of-type(' + numSlotsFilled + ')');
-            lastSlot.text('____');
-            numSlotsFilled--;
-        }
+    $('.number').bind('touchend', function(evt) { 
+    	log_tap_end(evt); 
     });
 
     /// FastClick CODE /// 
@@ -114,6 +72,16 @@ $(document).ready(function(){
     //                 console.log('attempt failed to save');
     //             }
     //         });
+    //     }
+    // });
+
+    // /// CODE FOR HANDLING BACKSPACE /// 
+    // $('#backspace', context).click(function() {            //Change to fastButton
+    //     // get the last slot that was filled in and clear it
+    //     if (numSlotsFilled !== 0) {
+    //         var lastSlot = $('#input_circles li:nth-of-type(' + numSlotsFilled + ')');
+    //         lastSlot.text('____');
+    //         numSlotsFilled--;
     //     }
     // });
 
