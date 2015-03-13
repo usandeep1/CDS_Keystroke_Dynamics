08cdd4fb9a56f82c84e775d066ce1eb9367738bf
Sandeep Uppaluri <sandeep@dhcp-hol-1765.eduroam.cornell.edu>
2015-03-13 17:04:51 -0400

    test

--- js/androidscript.js
+++ js/androidscript.js
@@ -11,13 +11,15 @@ $(document).ready(function(){
     
     function log_tap_start(evt) 
     {
-        keyRect = evt.originalEvent.target.getBoundingClientRect(),
-        offset   = keyRect.top - docRect.top;
+        var docRect = document.body.getBoundingClientRect()
+        var keyRect = evt.originalEvent.target.getBoundingClientRect(),
     	//console.log ( 'evt.originalEvent.target.id: ' + evt.originalEvent.target.id );
         //console.log ('$(this).offset().top: ' + $("button").position());
     	// console.log ( 'evt.originalEvent.target.position().left: ' + evt.originalEvent.target.position().left );
         console.log ('keyRect.top' + keyRect.top);
         console.log ('keyRect.left' + keyRect.left);
+        console.log ('docRect.top' + docRect.top);
+        console.log ('docRect.left' + docRect.left);
     	console.log ( 'evt.originalEvent.touches[0].pageX: ' + evt.originalEvent.touches[0].pageX );   //maybe change 0 to length - 1 ?????
         console.log ( 'evt.originalEvent.touches[0].pageY: ' + evt.originalEvent.touches[0].pageY );   //maybe change 0 to length - 1 ?????
   		// var x = elem.offset().left; //evt.originalEvent.pageX - evt.target.offset().left;
