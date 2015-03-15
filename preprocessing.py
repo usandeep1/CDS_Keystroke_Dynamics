1827b3665b26e7e44eb9f82497eb7b57d6034497
Akhila Ananthram <asa225@cornell.edu>
2015-03-15 15:58:10 -0400

    identify valid clicks

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 class User:
     def __init__(self, id, password, trials=None):
@@ -13,14 +14,25 @@ class User:
 
 class Trial:
     def __init__(self, **kwargs):
-        self.start = kwargs['start']
-        self.end = kwargs['end']
-        self.button_name = kwargs['button_name']
-        self.accel_x = kwargs['accel_x']
-        self.accel_y = kwargs['accel_y']
-        self.accel_z = kwargs['accel_z']
-        self.loc_x = kwargs['loc_x']
-        self.loc_y = kwargs['loc_y']
+        self.start = np.array(kwargs['start'])
+        self.end = np.array(kwargs['end'])
+        self.button_name = np.array(kwargs['button_name'])
+        self.accel_x = np.array(kwargs['accel_x'])
+        self.accel_y = np.array(kwargs['accel_y'])
+        self.accel_z = np.array(kwargs['accel_z'])
+        self.loc_x = np.array(kwargs['loc_x'])
+        self.loc_y = np.array(kwargs['loc_y'])
+        self.valid = self._valid_clicks()
+
+    def _valid_clicks(self):
+        valid = np.ones(len(self.start), dtype=bool)
+        for i in xrange(len(self.start)):
+            if self.button_name[i] == 'delete':
+                valid[i] = False
+                if i > 0:
+                    valid[i - 1] = False
+
+        return valid
 
 def read_data(filename):
     datatable = {}
