360ad049da2ff6035b1f1a17a970547aa45bae8b
Sandeep Uppaluri <sandeep@dhcp-rhodes-721.redrover.cornell.edu>
2015-03-16 04:44:48 -0400

    small changes

--- js/loginscript.js
+++ js/loginscript.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
     Parse.initialize("SBYhfUbIzN4jFYOYCg5acXyhZ6DAZjEMPmxPhcZM", "ACJPWbfahWN1jT40NUKjtJgJhJlBaQw4NvHe4q0k");
-    
+    $("#container").css("overflow-x:hidden; overflow-y:scroll;");
+
 });
