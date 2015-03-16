52f7eee4beea62b6367d165c2a675be28b13375e
Sandeep Uppaluri <sandeep@dhcp-rhodes-721.redrover.cornell.edu>
2015-03-16 05:14:00 -0400

    fixes to signup and login

--- js/loginscript.js
+++ js/loginscript.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
     Parse.initialize("SBYhfUbIzN4jFYOYCg5acXyhZ6DAZjEMPmxPhcZM", "ACJPWbfahWN1jT40NUKjtJgJhJlBaQw4NvHe4q0k");
     $("#container").css("overflow-x:hidden; overflow-y:scroll;");
     
-    function register_new_user(form) { 
+    $("#register_user_btn").on("click",function(){
+    // function register_new_user(form) { 
+        var form = event.originalEvent.target.form; 
         var user = new Parse.User();
         user.set("username", form.username.value);
         user.set("password", form.pwd.value);
@@ -18,7 +20,9 @@ $(document).ready(function(){
         });
     }
 
-    function login_user(form) { 
+    $("#register_user_btn").on("click",function(){
+    // function login_user(form) { 
+        var form = event.originalEvent.target.form; 
         Parse.User.logIn(form.userid.value, form.pswrd.value, {
           success: function(user) {
             window.open('www.usandeep1.github.io/index.html')
