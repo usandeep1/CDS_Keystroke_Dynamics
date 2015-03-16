b4aa372487c5d99b071ac81c884af9fb0fc18ac3
Sandeep Uppaluri <sandeep@dhcp-mcfad-345.eduroam.cornell.edu>
2015-03-15 20:48:15 -0400

    test

--- js/androidscript.js
+++ js/androidscript.js
@@ -3,6 +3,12 @@ $(document).ready(function(){
     Parse.initialize("SBYhfUbIzN4jFYOYCg5acXyhZ6DAZjEMPmxPhcZM", "ACJPWbfahWN1jT40NUKjtJgJhJlBaQw4NvHe4q0k");
     var Attempt = Parse.Object.extend("Attempt");
     
+    window.ondevicemotion = function(event) {
+        console.log ('accelerationIncludingGravity.x: ' + event.accelerationIncludingGravity.x)
+        console.log ('accelerationIncludingGravity.y: ' + event.accelerationIncludingGravity.y)
+        console.log ('accelerationIncludingGravity.z: ' + event.accelerationIncludingGravity.z)
+    }
+
     var attempt = new Attempt();
     attempt.set('start_times', [])
     attempt.set('end_times', [])
@@ -24,7 +30,7 @@ $(document).ready(function(){
 
     var numSlotsFilled = 0;
     var context = $('#passcode');
-    
+
     $('.number').bind('touchstart', function(evt) { 
     	log_tap_start(evt); 
     });
@@ -44,59 +50,16 @@ $(document).ready(function(){
             
             // print a success message on completion
             $('#successMsg', context).text('Thanks!');
-            // attempt.save(null, {
-            //     success: function(object) {
-            //         console.log('attempt successfully saved');
-            //     },
-            //     error: function(model, error) {
-            //         console.log('attempt failed to save');
-            //     }
-            // });
+            attempt.save(null, {
+                success: function(object) {
+                    console.log('attempt successfully saved');
+                },
+                error: function(model, error) {
+                    console.log('attempt failed to save');
+                }
+            });
         } 
     });
-
-    /// FastClick CODE /// 
-    // $(function() {
-    //     FastClick.attach(document.body);
-    // });
-
-    // /// CODE FOR PREVENTING SCROLLING
-    // document.body.addEventListener('touchstart', function(e){ 
-    //     e.preventDefault(); 
-    // });
-
-    /// CODE FOR FILLING IN SLOTS ///
-    
-    // establish context for faster search
-    // var context = $('#passcode');
-    
-    // $('.number', context).click(function() {      //Change to fastButton
-
-    //     // clear the success message if it is there
-        // $('#successMsg', context).text('');
-        
-        // // fill in the next empty slot with a circle
-        // var nextSlot = $('#input_circles li:nth-of-type(' + (numSlotsFilled + 1) + ')');
-        // nextSlot.text('O');
-        // numSlotsFilled++;
-        
-        // // clear all the slots if the passcode is complete
-        // if (numSlotsFilled === 4){
-        //     $('#input_circles li').text('____');
-        //     numSlotsFilled = 0;
-            
-        //     // print a success message on completion
-        //     $('#successMsg', context).text('Thanks!');
-        //     attempt.save(null, {
-        //         success: function(object) {
-        //             console.log('attempt successfully saved');
-        //         },
-        //         error: function(model, error) {
-        //             console.log('attempt failed to save');
-        //         }
-        //     });
-        // }
-    // });
 
     /// CODE FOR HANDLING BACKSPACE /// 
     $('#backspace').bind('touchend', function(evt) { 
